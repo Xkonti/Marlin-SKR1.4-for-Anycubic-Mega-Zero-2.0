284f6b72b534ba099934f37cffc17dd31a95ea62
Xkonti <online.xkonti@gmail.com>
2021-04-11 23:18:11 -0400

    Update settings for the direct extruder conversion

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2333,7 +2333,7 @@
   #define INTERPOLATE      true
 
   #if AXIS_IS_TMC(X)
-    #define X_CURRENT       800        // (mA) RMS current. Multiply by 1.414 for peak current.
+    #define X_CURRENT       840        // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     32        // 0..256
     #define X_RSENSE          0.11
@@ -2405,7 +2405,7 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      700
+    #define E0_CURRENT      570
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
